e26f51a6289ba30dab0b6cc395c2687846a84a10
Empleos cali 2026 <carlosmtl352@gmail.com>
2026-01-08 15:07:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.set_page_config(page_title="Empleos Cali 2025")
+
+st.title("Generador de imágenes de empleo")
+st.write("Empleos Cali 2025")
+
+st.text_input("Cargo")
+st.text_input("Ciudad")
+st.text_input("Tipo de contrato (opcional)")
+
+st.button("Generar imagen")
